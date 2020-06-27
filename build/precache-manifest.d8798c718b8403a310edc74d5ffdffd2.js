77b5f095b687d87f2fa1e151f5135f7ad7c341fd
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2020-06-27 19:56:49 +0100

    second react commit to add build folder

---
+++ build/precache-manifest.d8798c718b8403a310edc74d5ffdffd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c222ff4f7b3fbbf2ff4f788097f002c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92e50e81aba64dc17ec6",
+    "url": "/static/css/main.d86a9b7f.chunk.css"
+  },
+  {
+    "revision": "35bc4b184d753c12237b",
+    "url": "/static/js/2.076cb553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.076cb553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e50e81aba64dc17ec6",
+    "url": "/static/js/main.fed6a634.chunk.js"
+  },
+  {
+    "revision": "be5be4ff76595305b54b",
+    "url": "/static/js/runtime-main.4c71214b.js"
+  }
+]);
